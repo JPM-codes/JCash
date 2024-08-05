e049609b991032098240d1d7ed427206804aa981
JoaoMello20 <mellojoaopedro20@gmail.com>
2024-08-04 21:24:12 -0300

    Commit

--- src/main/java/me/jp/jcash/command/CashCommand.java
+++ src/main/java/me/jp/jcash/command/CashCommand.java
@@ -1,19 +1,26 @@
 package me.jp.jcash.command;
 
+import lombok.RequiredArgsConstructor;
 import lombok.val;
+import me.jp.jcash.data.CashAccount;
+import me.jp.jcash.data.CashAccountCache;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+@RequiredArgsConstructor
 public class CashCommand {
+
+    private final CashAccountCache cashAccountCache;
 
     @Command(
             name = "cash"
     )
     public void execute(Context<CommandSender> context) {
         val player = (Player) context.getSender();
-
+        CashAccount userAccount = cashAccountCache.getByUUID(player.getUniqueId());
+        player.sendMessage("§aCash: §f" + userAccount.getAmount());
     }
 
 }
